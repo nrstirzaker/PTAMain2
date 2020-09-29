de54ca69024b06ac4dd99a7f5909fa7dc16990cc
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2020-09-29 22:30:03 +0100

    Update to font page, dates now included. 2nd hand uniform page partially done

--- server/pages/index.vue
+++ server/pages/index.vue
@@ -8,33 +8,48 @@
           <p>
             The Friend's of Christ's College, the school's PTA,
             is a community of parents working to enrich the education of all the students.
-            All money raised is fed back into the pupils
+            <span class="font-bold">All money raised is used to provide for the pupils</span>
           </p>
         </div>
 
-        <div class="flex mt-4">
-          <p>
-            The current Committee members are as follows:
-          </p>
-        </div>
 
-        <div class="flex w-1/2">
-          <table class="table-auto">
-            <thead>
-            <tr>
-              <th class="w-1/2">Role</th>
-              <th class="w-1/2">Name</th>
-            </tr>
-            </thead>
-            <tbody>
-            <div v-for="member in members">
+        <div class="flex">
+          <div class="w-1/4">
+            <div class="mt-4">
+              The current committee members are:
+            </div>
+
+            <table class="table-fixed">
+              <thead>
               <tr>
+                <th class="w-1/2">Role</th>
+                <th class="w-1/2">Name</th>
+              </tr>
+              </thead>
+              <tbody>
+
+              <tr v-for="member in members">
                 <td class="border px-4 py-2">{{ member.role }}</td>
                 <td class="border px-4 py-2">{{ member.name }}</td>
               </tr>
+
+              </tbody>
+            </table>
+          </div>
+          <div class="w-1/2">
+            <div class="mt-4 text-center">
+              Calendar
             </div>
-            </tbody>
-          </table>
+            <div class="mt-4">
+              Next Meeting <span class="font-bold" v-text="nextMeeting()"/>
+              <div>(Due to the 'Rule of 6' we are meeting on-line. If you have ideas or questions please email us for connections details)</div>
+            </div>
+
+            <div class="mt-4">
+              Next 2nd Hand Uniform Sale <span class="font-bold" v-text="next2ndHandUniformSale()"/>
+            </div>
+
+          </div>
         </div>
 
         <div class="flex mt-4">
@@ -46,12 +61,6 @@
           If you would like to know more about joining the Committee or becoming a “helper” please contact us on the email below as there are numerous ways to help!
         </div>
 
-        <div class="flex mt-4">
-          If you would like to pop in and share your ideas our next committee meeting is on {}, at Worpleston Place
-        </div>
-        <div class="flex mt-4">
-          Our next 2nd Hand Uniform Sale is on {}
-        </div>
 
       </div>
     </div>
@@ -59,24 +68,70 @@
 </template>
 
 <script>
-
+import AdvancedFormat from 'dayjs/plugin/advancedFormat'
+import dayjs from 'dayjs'
 
 export default {
   layout: 'default',
   name: "index.vue",
+  mounted() {
+    dayjs.extend(AdvancedFormat)
+  },
   data() {
     return {
       members: [
         {"index": 1, "role": "Chair", "name": "Nigel Stirzaker"},
         {"index": 2, "role": "Treasurer", "name": "Sarah Hedger Howe"},
         {"index": 3, "role": "Secretary", "name": "Margret Mayersbeth"},
-        {"index": 4, "role": "2nd Hand Uniform", "name": "Sandra Phillips"},
+        {"index": 4, "role": "2nd Hand Uniform Sales", "name": "Sandra Phillips"},
         {"index": 5, "role": "Member", "name": "Hanna Morris"},
         {"index": 6, "role": "Member", "name": "Sylvia Owen"},
         {"index": 7, "role": "Teacher", "name": "Stuart Kennedy"},
         {"index": 8, "role": "Teacher", "name": "Zed Annan"},
       ]
     }
+  },
+  methods: {
+    next2ndHandUniformSale: function () {
+      const dates = [
+        new Date("2020-09-25"),
+        new Date("2020-10-23"),
+        new Date("2020-11-27"),
+        new Date("2021-01-29"),
+        new Date("2021-02-26"),
+        new Date("2021-03-26"),
+        new Date("2021-04-30"),
+        new Date("2021-05-28"),
+        new Date("2021-06-25"),
+        new Date("2021-07-15")
+      ];
+
+      const nextDate = dayjs(dates.find(date => (date > new Date()))).format('dddd Do MMMM');
+
+      return nextDate;
+    },
+    nextMeeting: function () {
+
+      const dates = [
+        new Date("2020-10-07"),
+        new Date("2020-11-11"),
+        new Date("2020-12-09"),
+        new Date("2021-01-13"),
+        new Date("2021-02-03"),
+        new Date("2021-03-10"),
+        new Date("2021-04-21"),
+        new Date("2021-05-12"),
+        new Date("2021-06-16"),
+        new Date("2021-07-14")
+
+      ];
+
+      const nextDate = dayjs(dates.find(date => (date > new Date()))).format('dddd Do MMMM');
+
+      return nextDate;
+
+    }
+
   }
 
 }
--- server/pages/main/second-hand-uniform.vue
+++ server/pages/main/second-hand-uniform.vue
@@ -12,26 +12,42 @@
       Boys
     </div>
 
-    <table class="table-fixed">
-      <thead>
-      <tr>
-        <th class="w-1/5 px-4 py-2">Blazer</th>
-        <th class="w-1/5 px-4 py-2">Shirt</th>
-        <th class="w-1/5 px-4 py-2">Jumper</th>
-        <th class="w-1/5 px-4 py-2">Tie</th>
-        <th class="w-1/5 px-4 py-2">Trousers</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td class="border px-4 py-2">£9</td>
-        <td class="border px-4 py-2">£4</td>
-        <td class="border px-4 py-2">£7</td>
-        <td class="border px-4 py-2">£2</td>
-        <td class="border px-4 py-2">£1</td>
-      </tr>
-      </tbody>
-    </table>
+
+    <div class="flex">
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Boys-Blazer-400x400.jpg"/>
+        <div class="text-center">Boys Blazer</div>
+        <div class="text-center">£10</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Shirt-400x400.jpg"/>
+        <div class="text-center">Single Shirt</div>
+        <div class="text-center">£4</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Tie-400x400.jpg"/>
+        <div class="text-center">Tie</div>
+        <div class="text-center">£2</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Jumper-400x400.jpg"/>
+        <div class="text-center">Jumper</div>
+        <div class="text-center">£5</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Trousers-400x400.jpg"/>
+        <div class="text-center">Trousers</div>
+        <div class="text-center">£3</div>
+      </div>
+
+
+    </div>
+
 
     <br>
 
@@ -39,30 +55,55 @@
       Girls
     </div>
 
-    <table class="table-fixed">
-      <thead>
-      <tr>
-        <th class="w-1/5 px-4 py-2">Blazer</th>
-        <th class="w-1/5 px-4 py-2">Blouse</th>
-        <th class="w-1/5 px-4 py-2">Cardigan</th>
-        <th class="w-1/5 px-4 py-2">Skirt</th>
-        <th class="w-1/5 px-4 py-2">Trousers</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td class="border px-4 py-2">£9</td>
-        <td class="border px-4 py-2">£4</td>
-        <td class="border px-4 py-2">£7</td>
-        <td class="border px-4 py-2">£5</td>
-        <td class="border px-4 py-2">£4</td>
-      </tr>
-      </tbody>
-    </table>
+    <div class="flex">
 
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Girls-Blazer-400x400.jpg"/>
+        <div class="text-center">Girls Blazer</div>
+        <div class="text-center">£10</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Blouse-400x400.jpg"/>
+        <div class="text-center">Single Blouse</div>
+        <div class="text-center">£4</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Cardi-400x400.jpg"/>
+        <div class="text-center">Cardigan</div>
+        <div class="text-center">£5</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Skirt-400x400.jpg"/>
+        <div class="text-center">Skirt</div>
+        <div class="text-center">£5</div>
+      </div>
+
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Trousers-400x400.jpg"/>
+        <div class="text-center">Trousers</div>
+        <div class="text-center">£3</div>
+      </div>
+
+
+    </div>
 
     <div class="text-2xl">
-      PE Kit
+      General
+    </div>
+
+    <div class="flex">
+      <div>
+        <img class="h-48 w-48" src="~/assets/images/Rucksack-400x400.jpg"/>
+        <div class="text-center">Rucksack</div>
+        <div class="text-center">£8</div>
+      </div>
+    </div>
+
+    <div class="text-2xl">
+      PE Kit Boys
     </div>
 
     <table class="table-fixed">
@@ -96,7 +137,7 @@
 
 
   </div>
-  
+
 </template>
 
 <script>
