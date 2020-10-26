bb0b736546f4142d353d9b9342859d263a762f9e
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2020-10-26 17:56:45 +0000

    Update Members page

--- server/pages/main/members.vue
+++ server/pages/main/members.vue
@@ -8,19 +8,48 @@
     <div class='container'>
 
       <div class="member">
-        <div class="flex">
-          <img src="~/assets/images/members/Sandra.jpg" class="h-48 w-48" alt="Sandra Philips">
-          <div>
-            Hi my name is Sandra Phillips. I have 2 boys at Christ's, they are currently in years 7 and 9.
-            I have been involved with FCC for nearly 2 years. I help run the 2nd hand uniform sales along with Sarah and help at the events like summer festival serving
-            refreshments etc.
-            I was also involved in the boys previous school PTA, having been involved with both I can definitely say that there is less pressure in a secondary school PTA.
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Nigel.jpg" class="object-fit:contain w-full" alt="Nigel Stirzaker">
+          </div>
+          <div class="w-full ml-10">
+            Hi, my name is Nigel Stirzaker, I’m the current Chair of the Friend’s of Christ’s College, the School’s PTA. Why did I volunteer, why get involved in raising money for
+            this school or any school?
+
+            Why - to make things better, to give all our communities children more and better opportunities than I had when I was at school in the 70s and 80s. All of us as parents
+            want our children to have a better life than we’ve had and many of the opportunities for them to fly higher and further than we have, come via their education in its
+            various forms. Having a home, owned or rented, is a struggle these days as the world of work gets more competitive and it’s only going to get harder. The PTA’s role is
+            to generate money for activities and items that will enhance our children’s all-round education. We don’t tend to focus on the academic side of education but on the
+            soft skills that are so essential in getting along in the work environment. In the past, we have used the money to part-fund internet safety workshops, to hire coaches
+            to take whole year groups to a careers fairs and most recently have bought the school’s brand new football kit. All public schools are short of money, resources, staff
+            and have to work within complex bureaucratic frameworks that our successive governments have designed in an effort to improve things. As a PTA we get to identify needs
+            and provide funds to meet those needs. Why am I involved in the PTA? Because it allows me to play a part in making things better.
           </div>
         </div>
 
-        <div class="flex">
-          <img src="~/assets/images/members/Margareth.jpeg" class="h-48 w-48" alt="Margareth Mayersbeth">
-          <div>
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sarah.jpg" class="object-fit:contain w-full" alt="Sarah Howe">
+          </div>
+          <div class="w-full ml-10">
+            My name is Sarah Howe and I am Treasurer for Friends of Christ’s College. I have a son, Oliver, who is currently in Year 8 at Christ’s College and I have a younger son,
+            Cameron, who will be joining Year 7 in September 2020.
+            I joined the Christ’s College Committee in 2019 as I have a passion for supporting schools where there is a greater need for fundraising and parent support. Currently
+            there are only 5 parent members on the Committee so we are very small, and we would really like to encourage more parents to join our friendly team. I have been on the
+            Committee for Weyfield Primary Academy School for several years so have a lot of fundraising experience. For a Secondary School it is far harder to raise money, because
+            as parents we are far more removed, but our small but perfectly formed team, are trying to engage with parents and students to help make the Friends of Christs College
+            be part of the school community and help raise money to benefit all the children at the school.
+            I come from a PA background so have a lot of experience in organisation and administration, but my key asset is being a people person, so please don’t hesitate to speak
+            to me if you feel you would like to join the team or help in anyway as I am very approachable!
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Margareth.jpg" class="object-fit:contain w-full" alt="Margareth Mayersbeth">
+          </div>
+          <div class="w-full ml-10">
             Hi my name is Margareth Mayersbeth, I have two children at Christ's college, one started in September 2017, the other in 2019.
             I have helped FCC since November 2019 and it has been really great to meet monthly at Worplesdon Place with the FCC team to come up with ideas for building up our
             school's community and to raise funds. I have taken on the secretarial duties for FCC.
@@ -33,9 +62,68 @@
           </div>
         </div>
 
-        <div class="flex">
-          <img src="~/assets/images/members/Stuart.jpeg" class="h-48 w-48" alt="Stuart Kennedy">
-          <div>
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sandra.jpg" class="object-fit:contain  w-full" alt="Sandra Philips">
+          </div>
+          <div class="w-full ml-10">
+            Hi my name is Sandra Phillips. I have 2 boys at Christ's, they are currently in years 7 and 9.
+            I have been involved with FCC for nearly 2 years. I help run the 2nd hand uniform sales along with Sarah and help at the events like summer festival serving
+            refreshments etc.
+            I was also involved in the boys previous school PTA, having been involved with both I can definitely say that there is less pressure in a secondary school PTA.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Stuart.jpg" class="object-fit:contain w-full" alt="Stuart Kennedy">
+          </div>
+          <div class="w-full ml-10">
+            My name is Stuart Kennedy. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Zed.jpg" class="object-fit:contain w-full" alt="Zed Annan">
+          </div>
+          <div class="w-full ml-10">
+            Hi My name is Zed Annan and I have worked at Christ's College for almost 4 years.
+            I am the Student Development Lead which incorporates lots of different aspects of the school, including leading on careers and mental health & Wellbeing. I am also a
+            part of the safeguarding team.
+            Having only recently joined the FCC I can honestly say how welcoming they all are. The sense of community is one of the many things that makes me love working at CCG
+            and the FCC are always supportive and wanting to do the best for the school. It Is always great to have new people join!
+            Look forward to seeing some new faces at our monthly meetings!
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Hanna.jpg" class="object-fit:contain  w-full" alt="Hanna Morris">
+          </div>
+          <div class="w-full ml-10">
+            My name is Hanna Morris. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sylvia.jpg" class="object-fit:contain  w-full" alt="Sylvia Owen">
+          </div>
+          <div class="w-full ml-10">
+            My name is Sylvia Owen. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
         </div>
 
