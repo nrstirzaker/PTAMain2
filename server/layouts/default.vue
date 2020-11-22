2243eae38292531a9e3824c29624532917def0a7
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2020-11-22 14:38:07 +0000

    Release 1.1.4 Made template size to full "screen"

--- server/layouts/default.vue
+++ server/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class='flex flex-col min-h-screen h-screen'>
+  <div class='flex flex-col max-h-full'>
 
     <Header></Header>
     <div class="flex flex-row flex-grow">
@@ -16,21 +16,21 @@
 
 <script>
 
-  import Header from '../components/Header'
-  import SideNavBar from '../components/SideNavBar'
-  import ContactAndMediaFooter from '../components/ContactAndMediaFooter'
-  import LegalFooter from '../components/LegalFooter'
+import Header from '../components/Header'
+import SideNavBar from '../components/SideNavBar'
+import ContactAndMediaFooter from '../components/ContactAndMediaFooter'
+import LegalFooter from '../components/LegalFooter'
 
 
-  export default {
-    components: {Header, SideNavBar, ContactAndMediaFooter, LegalFooter},
-  }
+export default {
+  components: {Header, SideNavBar, ContactAndMediaFooter, LegalFooter},
+}
 </script>
 
 <style>
-  body {
-    @apply text-black bg-white
-  }
+body {
+  @apply text-black bg-white
+}
 </style>
 
 
