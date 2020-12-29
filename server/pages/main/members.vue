1e5ce44840f061d823f94579e5a9beebfdba5714
Nigel Stirzaker <nrstirzaker.techblog@gmail.com>
2020-12-29 22:00:56 +0000

    Heroku does to keep saved Strapi images, quick hack to get site working again

--- server/pages/main/members.vue
+++ server/pages/main/members.vue
@@ -15,7 +15,7 @@
           >
             <div class="flex m-10">
               <div class="w-48">
-                <img v-bind:src="getMemberImageURL(member)" class="object-fit:contain w-full" :alt=member.name>
+                <img v-bind:src="getMemberImageURL(member.visual_order)" class="object-fit:contain w-full" :alt=member.name>
               </div>
               <div class="w-full ml-10">
                 <div v-html="$md.render(member.bio)"></div>
@@ -31,13 +31,22 @@
 
 <script>
 import axios from 'axios';
+import Nigel from '~/assets/images/members/Nigel.jpg';
+import Sarah from '~/assets/images/members/Sarah.jpg';
+import Margareth from '~/assets/images/members/Margareth.jpg';
+import Sandra from '~/assets/images/members/Sandra.jpg';
+import Hanna from '~/assets/images/members/Hanna.jpg';
+import Sylvia from '~/assets/images/members/Sylvia.jpg';
+import Stuart from '~/assets/images/members/Stuart.jpg';
+import Zed from '~/assets/images/members/Zed.jpg';
 
 export default {
   name: "members.vue",
   layout: 'default',
   data() {
     return {
-      members: []
+      members: [],
+      images: []
     }
 
   },
@@ -45,12 +54,19 @@ export default {
     getCMSBaseURL: function () {
       return process.env.NUXT_ENV_STRAPI_BASE_URL;
     },
-    getMemberImageURL: function (member) {
-      if (member.photo) {
-        return this.getCMSBaseURL() + member.photo.url || '';
-      } else {
-        return '';
-      }
+    getMemberImageURL: function (id) {
+      const images = [
+        "Nigel.jpg",
+        "Sarah.jpg",
+        "Margareth.jpg",
+        "Sandra.jpg",
+        "Stuart.jpg",
+        "Zed.jpg",
+        "Hanna.jpg",
+        "Sylvia.jpg"
+      ]
+
+      return '/_nuxt/assets/images/members/' + images[id - 1];
     }
   },
   mounted() {
