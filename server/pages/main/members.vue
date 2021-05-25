c24a7850e1bab39463669d7b5ff5d9507ac99ed9
Nigel <nrstirzaker.techblog@gmail.com>
2021-05-25 20:45:58 +0100

    Update of node packages for security. And "reverted" members page to static

--- server/pages/main/members.vue
+++ server/pages/main/members.vue
@@ -8,23 +8,134 @@
     <div class='container'>
 
       <div class="member">
-        <ul>
-          <li
-            v-for="(member, key) in members"
-          >
-            <div class="flex m-10">
-              <div class="w-48">
-                <!--<img :src='items[key].src' class="object-fit:contain w-full" :alt=member.name>-->
-                <img v-bind:src='getMemberImageURL(key)' class="object-fit:contain w-full" :alt=member.name>
-              </div>
-              <div class="w-full ml-10">
-                <div v-html="$md.render(member.bio)"></div>
-              </div>
-            </div>
-          </li>
-        </ul>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Nigel.jpg" class="object-fit:contain w-full" alt="Nigel Stirzaker">
+          </div>
+          <div class="w-full ml-10">
+            Hi, my name is <span class="font-bold">Nigel</span> Stirzaker, I am the current chair of FCC, the PTA of Christ’s College. I volunteer because I want to raise funds
+            to give our children more opportunities than I had growing up in the 70s and 80s. I want them to be able to fly higher and further than we have. Having a home, owned or
+            rented, is a struggle these days as the world and work gets more and more competitive. The PTA’s role is to generate money for activities and items that will enhance
+            our children’s all-round education, not necessarily academic but more soft-skills which are essential to get along in the today’s work environments.
+            In the past, we have raised money to part-fund internet safety workshops, hire coaches to take year groups to a career fairs and most recently we have bought the
+            school’s brand-new football kit. All public schools are short of money, resources, staff and have to work within complex bureaucratic frameworks that our successive
+            governments have designed in an effort to improve things. As a PTA we try and identify needs and provide funds to meet those needs. So, why am I a part of the PTA?
+            Because it allows me to play a part
+            in making things better.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sarah.jpg" class="object-fit:contain w-full" alt="Sarah Howe">
+          </div>
+          <div class="w-full ml-10">
+            My name is <span class="font-bold">Sarah</span> Hedger-Howe, I am Treasurer for Friends of Christ’s College. I have a son who started in Christ’s College in
+            September
+            2018 and I have a younger son
+            who started in 2020.
+            I joined the Christ’s College Committee in 2019 as I have a passion for supporting schools where there is a greater need for fundraising and parent support. Currently
+            there are 6 parent members on the Committee. We always encourage more parents to join our friendly team. I have been on the Committee for Weyfield Primary Academy
+            School for several years so have a lot of fundraising experience. For a Secondary School it is harder to raise money, because as parents we are far more removed, but
+            our small team tries to engage with parents and students to help make the Friends of Christ’s College be a real part of the school community.
+            I come from a PA background so have a lot of experience in organisation and administration, but my key asset is being a people person, so please don’t hesitate to speak
+            to me if you feel you would like to join the team or help in anyway as I am very approachable!
+            You will find me at most 2nd Hand Uniform sales too where I help Sandra Philips of our team.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Margareth.jpg" class="object-fit:contain w-full" alt="Margareth Mayersbeth">
+          </div>
+          <div class="w-full ml-10">
+            Hi my name is <span class="font-bold">Margareth</span> Mayersbeth, I have two children at Christ’s College, one started in September 2017 and another in 2019. I
+            have
+            helped FCC since November 2019
+            and it has been really great to meet at Worplesdon Place with the FCC team to come up with ideas for building up our school’s community and to raise funds. I have taken
+            on the role of secretarial duties for FCC. I have recently become an exam invigilator and volunteer at the school’s library regularly during the day. Fortnightly I
+            enjoy attending Parents Prayer in the Chapel to pray for our school too. I have been a class representative in my children’s primary school and enjoy getting together
+            building up the school’s community. Nigel and I knew each other from Ripley Primary School, a local school which had to go through closure. It was a painful process and
+            this made me realize even more how important parental input, community, fundraising and positivity is for a school, no matter what size the school is.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sandra.jpg" class="object-fit:contain  w-full" alt="Sandra Philips">
+          </div>
+          <div class="w-full ml-10">
+            Hi my name is <span class="font-bold">Sandra</span> Philips. I have 2 boys at Christ’s College. One started in 2017 and the other it 2019. I have been involved in
+            FCC
+            since 2018. I help run the 2nd hand uniform sales along with Sarah and I help at events like the summer festival serving refreshments etc. I was also involved in my
+            boy’s previous school’s PTA too.
+            Having been involved in both I can definitely say that there is less pressure in a secondary school’s PTA.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Stuart.jpg" class="object-fit:contain w-full" alt="Stuart Kennedy">
+          </div>
+          <div class="w-full ml-10">
+            I joined the school in September 2000, so have been part of this community for 20yrs now. During that time I have been a full time teacher, head of department and now a
+            member of the leadership team. I have had several roles and responsibilities on the leadership team but one of my favourite things has been as the CLT link for Friends
+            of Christ’s College. I joined in 2012 having been asked by Mr Green to link FCC with the 6th Form. Little did I know the impact it would have on me.
+            I serve as a point of contact and help for FCC. Offering the college point of view or answering questions. I help with getting the student body to work with FCC and
+            between us we all work out what the staff or students need and I liaise with FCC to see how they can help.
+            Over the years FCC has raised so much money for the school and they have had a huge impact on education and students at the college. I get to see first hand the good
+            that they do for the students here and it is amazing how supportive they have been. From rewards schemes to lunchtime benches to 2nd hand uniform and internet safety
+            training...FCC fundraising events have provided a broad range of things that add value to the college.
+            Working with FCC has been a real pleasure and a privilege. It never seems like work to me, we have a lot of fun at our regular meets. They are informal but focussed. I
+            get to discuss school with parents and I learn a lot about the parents point of view. Often taking this information back to Mrs Hatch and the leadership team so we can
+            make improvements or change perceptions. FCC remains a really important part of the school and I could not recommend it highly enough.
+
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Zed.jpg" class="object-fit:contain w-full" alt="Zed Annan">
+          </div>
+          <div class="w-full ml-10">
+            Hi My name is <span class="font-bold">Zed</span> Annan and I have worked at Christ's College for almost 4 years.
+            I am the Student Development Lead which incorporates lots of different aspects of the school, including leading on careers and mental health & Wellbeing. I am also a
+            part of the safeguarding team.
+            Having only recently joined the FCC I can honestly say how welcoming they all are. The sense of community is one of the many things that makes me love working at CCG
+            and the FCC are always supportive and wanting to do the best for the school. It Is always great to have new people join!
+            Look forward to seeing some new faces at our monthly meetings!
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Hanna.jpg" class="object-fit:contain  w-full" alt="Hanna Morris">
+          </div>
+          <div class="w-full ml-10">
+            Hi, my name is <span class="font-bold">Hanna</span> Morris. I have two children at Christ’s College who joined in September 2020. I have a son in year 8 and a
+            daughter
+            in year 7. I have only recently
+            become involved with the PTA but help out wherever I can. I like to get involved and be part of the community, helping to raise important funds for the school.
+          </div>
+        </div>
+
+        <div class="flex m-10">
+          <div class="w-48">
+            <img src="~/assets/images/members/Sylvia.jpg" class="object-fit:contain  w-full" alt="Sylvia Owen">
+          </div>
+          <div class="w-full ml-10">
+            Hello I'm <span class="font-bold">Sylvia</span> Owen I have a child in year 7. I work with adults with a learning disabilities part-time and so I've always
+            been involved in helping Stoughton
+            and Northmead schools through helping at functions:school fairs, Christmas fairs and also in school with listening to children read. I feel it's so important to know
+            that your child knows you're involved in their lives and also get to know teachers and have fun. With Christ's College I hope that I can contribute in a small way and
+            good to feel involved in aspects of school life even through the pandemic way of working. Sylvia Owen
+          </div>
+        </div>
+
 
       </div>
+
     </div>
   </div>
 </template>
@@ -32,82 +143,18 @@
 <script>
 import axios from 'axios';
 
-
-//const images = require.context('../../static/images/members', true, /\.jpg$/);
-//const images = require.context('../../static/', false, /\.jpg$/);
-//this is used to ensure that the images are pulled into the build
-const images = require.context('../../assets/images/members', true, /\.jpg$/);
-// const imagesArray = Array.from(images.keys());
-// console.log(images);
-// console.log(imagesArray);
-
-// let constructed = [];
-//
-// function constructItems(fileNames, constructed) {
-//   fileNames.forEach(fileName => {
-//     console.log("filename.substr:" + fileName.substr(1))
-//     let nameKey = fileName.trim().substr(2, fileName.length - 6);
-//     console.log("nameKey:" + nameKey);
-//     let memberImageData = {[nameKey]: {'src': '../../_nuxt/assets/images/members' + fileName.substr(1)}}
-//     constructed.push(memberImageData);
-//   });
-//   return constructed;
-// };
-
-
-//let res = constructItems(imagesArray, constructed);
-
-
 export default {
   name: "members.vue",
+  //#ECC33D yellow
+  //#182F59 blue
   layout: 'default',
   data() {
     return {
-      members: []
-    }
-
-  },
-  methods: {
-    getCMSBaseURL: function () {
-      return 'https://fcc-strapi.herokuapp.com';
-    },
-    getMemberImageURL: function (id) {
-      const images = [
-        "Nigel.jpg",
-        "Sarah.jpg",
-        "Margareth.jpg",
-        "Sandra.jpg",
-        "Stuart.jpg",
-        "Zed.jpg",
-        "Hanna.jpg",
-        "Sylvia.jpg"
-      ]
-      return '../../_nuxt/assets/images/members/' + images[id];
-    }
+      members: [],
+    };
   },
   mounted() {
-
-    axios.get(
-      this.getCMSBaseURL() + "/members"
-      ,
-      {
-        params: {
-          _sort: 'visual_order:ASC'
-        }
-      }
-    )
-      .then(response => {
-        this.members = response.data;
-        // let membersArray = [];
-        // membersArray = response.data;
-        // membersArray.forEach(member => {
-        //   member.firstName = member.name.split(" ")[0]
-        //   this.members.push(member);
-        // })
-
-      })
   }
-
 }
 </script>
 
