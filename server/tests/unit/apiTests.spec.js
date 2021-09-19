46b88ecf396086803eae22c15a4a5d963aceccf3
Nigel <nrstirzaker.techblog@gmail.com>
2021-09-19 16:26:35 +0100

    package-lock.json updated and test update

--- server/tests/unit/apiTests.spec.js
+++ server/tests/unit/apiTests.spec.js
@@ -19,14 +19,38 @@ describe('Books', () => {
   /*
     * Test the /GET route
     */
-  describe('/GET book', () => {
-    it('it should GET all the books', (done) => {
+  describe('/GET Member', () => {
+    it('it should GET all Member and validate the first is Chair', (done) => {
       chai.request('https://fcc-strapi.herokuapp.com')
-        .get('/members')
+        .get('/Members')
         .end((err, res) => {
           res.should.have.status(200);
-          //res.body.should.be.a('array');
-          //res.body.length.should.be.eql(0);
+          res.body.should.be.instanceOf(Array);
+          res.body.length.should.be.greaterThan(0);
+          let user = res.body[0];
+          user.should.have.property('Role').which.is.eql('Chair');
+          user.should.have.property('Visual_Order').which.is.eql(1);
+          done();
+        });
+    });
+  });
+
+  describe('/GET Meeting Dates', () => {
+    it('it should GET all meeting dates. There should be one in the future', (done) => {
+      chai.request('https://fcc-strapi.herokuapp.com')
+        .get('/Meeting-Dates')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.instanceOf(Array);
+          res.body.length.should.be.greaterThan(0);
+          let dates = res.body;
+          let mostRecentDate = new Date();
+          for (let i = 0; i < dates.length; i++) {
+            if (dates[i] > mostRecentDate) {
+              mostRecentDate = dates[i];
+            }
+          }
+          mostRecentDate.should.have.greaterThan(new Date());
           done();
         });
     });
